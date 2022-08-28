7fedd8b01a4746c1fa4a5dc7a06893c50cab236b
liuk <kekeliu_job@163.com>
2022-08-28 10:17:32 +0800

    测试

---
+++ js/index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: liuk kekeliu_job@163.com
+ * @Date: 2022-08-28 10:16:31
+ * @LastEditors: liuk kekeliu_job@163.com
+ * @LastEditTime: 2022-08-28 10:17:18
+ * @FilePath: /XieCheng-Project/js/index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+console.log('我是刘可');
