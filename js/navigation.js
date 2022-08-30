284f18861c7032d37dfbba5965503d045790d800
liuk <kekeliu_job@163.com>
2022-08-30 20:39:04 +0800

    导航页讲解

--- js/navigation.js
+++ js/navigation.js
@@ -1,5 +1,18 @@
-const left =document.querySelectorAll(".navigation-middle");
-const click = document.getElementById("navigation");
-click.onclick = ()=>{
-    click.style.width='5%';
+const navBtn = document.getElementById('nav-btn');
+const navigation = document.getElementById('navigation');
+const navTitle = document.getElementsByClassName('title'); // [dom, dom, dom, dom]
+let switchNav = false; // false -> 代表小尺寸; true -> 代表大尺寸;
+navBtn.onclick = () => {
+    if (switchNav) {
+        navigation.className = 'nav-size-small';
+        for(let i = 0; i < navTitle.length; i++) {
+            navTitle[i].style.opacity = '0';
+        }
+    } else {
+        navigation.className = 'navigation-box';
+        for(let i = 0; i < navTitle.length; i++) {
+            navTitle[i].style.opacity = '1';
+        }
+    }
+    switchNav = !switchNav;
 }
