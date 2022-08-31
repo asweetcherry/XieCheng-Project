dc77c9cf01d5db6631b5e0b9ef4e1d7da8a382e1
asweetcherry <840927735@qq.com>
2022-08-31 23:25:08 +0800

    左侧导航条2.1版本

--- js/navigation.js
+++ js/navigation.js
@@ -1,16 +1,32 @@
 const navBtn = document.getElementById('nav-btn');
 const Ts1 = document.getElementById('hotel');
+const Ts2 = document.getElementById('plane');
+const Ts3 = document.getElementById('train');
+const Ts4 = document.getElementById('travel');
+const Ts5 = document.getElementById('boat');
+const Ts6 = document.getElementById('car');
+const Ts7 = document.getElementById('ball');
+const Ts8 = document.getElementById('card');
 const Tips1 = document.getElementById('tips-1');
+const Tips2 = document.getElementById('tips-2');
+const Tips3 = document.getElementById('tips-3');
+const Tips4 = document.getElementById('tips-4');
+const Tips5 = document.getElementById('tips-5');
+const Tips6= document.getElementById('tips-6');
+const Tips7= document.getElementById('tips-7');
+const Tips8 = document.getElementById('tips-8');
 const Tips = document.getElementsByClassName('tips');
 const navigation = document.getElementById('navigation');
 const navTitle = document.getElementsByClassName('title'); // [dom, dom, dom, dom]
 let switchNav = false; // false -> 代表小尺寸; true -> 代表大尺寸;
+const AllTs = [Ts1,Ts2,Ts3,Ts4,Ts5,Ts6,Ts7,Ts8];
+const AllTips =[Tips1,Tips2,Tips3,Tips4,Tips5,Tips6,Tips7,Tips8];
 for(let i = 0; i < Tips.length; i++) {
     Tips[i].style.opacity = '0';
 }
 Ts1.onmousemove= () =>{
-    Tips1.style.opacity="1";
-}
+    Tips1.style.opacity="1";}
+
 Ts1.onmouseleave = () =>{
     
     Tips1.style.opacity="0";
