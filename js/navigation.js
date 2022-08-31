4e06de0dde857de48d241fd3eab87f6183eeb7e7
asweetcherry <840927735@qq.com>
2022-08-31 22:32:28 +0800

    左侧导航条2.0版本

--- js/navigation.js
+++ js/navigation.js
@@ -1,17 +1,50 @@
 const navBtn = document.getElementById('nav-btn');
+const Ts1 = document.getElementById('hotel');
+const Tips1 = document.getElementById('tips-1');
+const Tips = document.getElementsByClassName('tips');
 const navigation = document.getElementById('navigation');
 const navTitle = document.getElementsByClassName('title'); // [dom, dom, dom, dom]
 let switchNav = false; // false -> 代表小尺寸; true -> 代表大尺寸;
+for(let i = 0; i < Tips.length; i++) {
+    Tips[i].style.opacity = '0';
+}
+Ts1.onmousemove= () =>{
+    Tips1.style.opacity="1";
+}
+Ts1.onmouseleave = () =>{
+    
+    Tips1.style.opacity="0";
+}
 navBtn.onclick = () => {
     if (switchNav) {
         navigation.className = 'nav-size-small';
         for(let i = 0; i < navTitle.length; i++) {
             navTitle[i].style.opacity = '0';
         }
+        navigation.onmouseleave = () =>{
+            navigation.className = 'nav-size-small';
+            for(let i = 0; i < navTitle.length; i++) {
+                navTitle[i].style.opacity = '0';
+            }
+        }
+        navigation.onmouseover = () =>{
+        navigation.className = 'navigation-box';
+        for(let i = 0; i < navTitle.length; i++) {
+            navTitle[i].style.opacity = '1';
+        }   
+        }
     } else {
         navigation.className = 'navigation-box';
         for(let i = 0; i < navTitle.length; i++) {
             navTitle[i].style.opacity = '1';
+        }
+        navigation.onmouseleave = () =>{
+            navigation.className = 'navigation-box';
+            
+        }
+        navigation.onmouseover = () =>{
+        navigation.className = 'navigation-box';
+                
         }
     }
     switchNav = !switchNav;
